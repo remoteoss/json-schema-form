86a74d3f504dc1a732a4fc5b9660227c7f0de41f
Gabriel <gabriel.garcia@remote.com>
2023-06-15 17:53:26 +0200

    can return undefined

--- json-schema-form.d.ts
+++ json-schema-form.d.ts
@@ -1,4 +1,4 @@
 /**
  * Shorthand to lookup for keys with `x-jsf-*` preffix.
  */
-export function pickXKey(node: Object, key: 'presentation' | 'error-message'): Object
+export function pickXKey(node: Object, key: 'presentation' | 'error-message'): Object | undefined
