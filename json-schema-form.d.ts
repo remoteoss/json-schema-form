47fbac0f7fe9db617368ce2805e9b17387b48684
Gabriel <gabriel.garcia@remote.com>
2023-06-16 10:31:16 +0200

    docs(typescript) - add declarations for createHeadlessForm

--- json-schema-form.d.ts
+++ json-schema-form.d.ts
@@ -1,20 +1,19 @@
 /**
  * Shorthand to lookup for keys with `x-jsf-*` preffix.
  */
-export function pickXKey(node: Object, key: 'presentation' | 'error-message'): Object | undefined
-
+export function pickXKey(node: Object, key: 'presentation' | 'error-message'): Object | undefined;
 
 type JSConfig = {
   initialValues?: Record<string, unknown>;
   strictInputType?: boolean;
   customProperties?: {
-    description: () => void | string;
+    description: Function | string;
     [key: string]: unknown;
-  }
+  };
   inputTypes?: {
     errorMessage: Record<string, unknown>;
-  }
-}
+  };
+};
 
 type Fields = Record<string, unknown>[]; // TODO: We don't know what type we have here, we need to investigate
 
@@ -25,4 +24,25 @@ type $TsFixMe = any;
  * These fields must be the same from
  * const { fields } = createHeadlessForm()
  */
-export function buildCompleteYupSchema(fields: Fields, config: JSConfig): $TsFixMe // TODO: We don't what yup returns here, we'll fix it later
+export function buildCompleteYupSchema(fields: Fields, config: JSConfig): $TsFixMe; // TODO: We don't what yup returns here, we'll fix it later
+
+type HeadlessFormOutput =
+  | {
+      fields: Fields;
+      handleValidation: Function;
+      isError: boolean;
+      error?: undefined;
+    }
+  | {
+      fields: never[];
+      isError: boolean;
+      error: any;
+    };
+
+/**
+ * Generates the Headless form based on the provided JSON schema
+ */
+export function createHeadlessForm(
+  jsonSchema: Record<string, unknown>,
+  customConfig: JSConfig = {}
+): HeadlessFormOutput;
