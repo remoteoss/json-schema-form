5c3d2169527d8627a817c2bf7d51e8efd7aa129d
Gabriel <gabriel.garcia@remote.com>
2023-06-16 10:01:23 +0200

    docs(json-schema) - add types for buildCompleteYupSchema

--- json-schema-form.d.ts
+++ json-schema-form.d.ts
@@ -2,3 +2,28 @@
  * Shorthand to lookup for keys with `x-jsf-*` preffix.
  */
 export function pickXKey(node: Object, key: 'presentation' | 'error-message'): Object | undefined
+
+
+type JSConfig = {
+  initialValues?: Record<string, unknown>;
+  strictInputType?: boolean;
+  customProperties?: {
+    description: () => void | string;
+    [key: string]: unknown;
+  }
+  inputTypes?: {
+    errorMessage: Record<string, unknown>;
+  }
+}
+
+type Fields = any; // TODO: We don't know what type we have here, we need to investigate
+
+type $TsFixMe = any;
+
+/**
+ * Returns the Yup schema structure of given fields.
+ * These fields must be the same from
+ * const { fields } = createHeadlessForm()
+ */
+
+export function buildCompleteYupSchema(fields: Fields, config: JSConfig): $TsFixMe // TODO: We don't what yup returns here, we'll fix it later
