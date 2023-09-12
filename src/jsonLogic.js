d3f26e2b8b5aaf3e35909e7f75279f519f8fa77d
brennj <john.brennan@remote.com>
2023-09-12 09:51:33 +0200

    parent 80c29589ac0972e0f33add70a59df15a46db1b43 author brennj <john.brennan@remote.com> 1694505093 +0200 committer brennj <john.brennan@remote.com> 1694508319 +0200
    Release 0.5.0-beta.0
    
    chore: support barebones computedAttrs
    
    chore: fix errors
    
    chore: fix mess ups from rebase
    
    chore: feedback from PR
    
    chore: pass logic down at updateFieldsProperties to prevent bugs
    
    feat: JSON Logic skeleton and plumbing setup
    
    chore: clean up conditional additions
    
    chore: remove const tests
    
    chore: remove dupe file
    
    chore: remove group array stuff
    
    chore: clean up yupschema
    
    chore: clean up helpers a small bit
    
    chore: remove all error handling for now
    
    chore: clean up package-lock
    
    chore: more removing stuff
    
    chore: clean more
    
    chore: support barebones computedAttrs
    
    chore: computed string attributes
    
    chore: fix tests
    
    chore: consistency for curly braces
    
    chore: remove unneeded code for now

--- src/jsonLogic.js
+++ src/jsonLogic.js
@@ -1,5 +1,7 @@
 import jsonLogic from 'json-logic-js';
 
+import { buildYupSchema } from './yupSchema';
+
 /**
  * Parses the JSON schema to extract the json-logic rules and returns an object
  * containing the validation scopes, functions to retrieve the scopes, and evaluate the
@@ -123,8 +125,25 @@ export function yupSchemaWithCustomJSONLogic({ field, logic, config, id }) {
     );
 }
 
+const HANDLEBARS_REGEX = /\{\{([^{}]+)\}\}/g;
+
+function replaceHandlebarsTemplates({
+  value: toReplace,
+  logic,
+  formValues,
+  parentID,
+  name: fieldName,
+}) {
+  if (typeof toReplace === 'string') {
+    return toReplace.replace(HANDLEBARS_REGEX, (match, key) => {
+      return logic.getScope(parentID).applyComputedValueInField(key.trim(), formValues, fieldName);
+    });
+  }
+  return toReplace;
+}
+
 export function calculateComputedAttributes(fieldParams, { parentID = 'root' } = {}) {
-  return ({ logic, formValues }) => {
+  return ({ logic, isRequired, config, formValues }) => {
     const { computedAttributes } = fieldParams;
     const attributes = Object.fromEntries(
       Object.entries(computedAttributes)
@@ -132,17 +151,55 @@ export function calculateComputedAttributes(fieldParams, { parentID = 'root' } =
         .filter(([, value]) => value !== null)
     );
 
-    return attributes;
+    return {
+      ...attributes,
+      schema: buildYupSchema(
+        { ...fieldParams, ...attributes, required: isRequired },
+        config,
+        logic
+      ),
+    };
   };
 }
 
 function handleComputedAttribute(logic, formValues, parentID) {
   return ([key, value]) => {
-    if (key === 'const')
+    if (key === 'description') {
+      return [key, replaceHandlebarsTemplates({ value, logic, formValues, parentID, name })];
+    }
+
+    if (key === 'title') {
+      return ['label', replaceHandlebarsTemplates({ value, logic, formValues, parentID, name })];
+    }
+
+    if (key === 'const') {
       return [key, logic.getScope(parentID).applyComputedValueInField(value, formValues)];
+    }
+
+    if (key === 'x-jsf-errorMessage') {
+      return [
+        'errorMessage',
+        handleNestedObjectForComputedValues(value, formValues, parentID, logic, name),
+      ];
+    }
 
     if (typeof value === 'string') {
       return [key, logic.getScope(parentID).applyComputedValueInField(value, formValues)];
     }
+
+    if (key === 'x-jsf-presentation' && value.statement) {
+      return [
+        'statement',
+        handleNestedObjectForComputedValues(value.statement, formValues, parentID, logic, name),
+      ];
+    }
   };
+}
+
+function handleNestedObjectForComputedValues(values, formValues, parentID, logic, name) {
+  return Object.fromEntries(
+    Object.entries(values).map(([key, value]) => {
+      return [key, replaceHandlebarsTemplates({ value, logic, formValues, parentID, name })];
+    })
+  );
 }
