43c5e058f6900bbf56d2eb5b8fb50cec1b2bef10
David Lange <lange@finiam.com>
2023-10-26 09:48:59 +0100

    update helper in test

--- src/tests/createHeadlessForm.test.js
+++ src/tests/createHeadlessForm.test.js
@@ -39,6 +39,7 @@ import {
   mockFileInput,
   mockRadioCardInput,
   mockRadioCardExpandableInput,
+  mockTelWithPattern,
   mockTextInput,
   mockTextInputDeprecated,
   mockNumberInput,
@@ -57,7 +58,6 @@ import {
   schemaInputTypeNumberWithPercentage,
   schemaForErrorMessageSpecificity,
   jsfConfigForErrorMessageSpecificity,
-  mockPatternOneOf,
 } from './helpers';
 import { mockConsole, restoreConsoleAndEnsureItWasNotCalled } from './testUtils';
 
@@ -1487,11 +1487,7 @@ describe('createHeadlessForm', () => {
     });
 
     it('supports oneOf pattern validation', () => {
-      const result = createHeadlessForm({
-        properties: {
-          phone_number: mockPatternOneOf,
-        },
-      });
+      const result = createHeadlessForm(mockTelWithPattern);
 
       expect(result).toMatchObject({
         fields: [
@@ -1507,7 +1503,19 @@ describe('createHeadlessForm', () => {
               },
               {
                 label: 'United Kingdom (UK)',
-                pattern: '^(\\+44)[0-9]*',
+                pattern: '^(\\+44)[0-9]{1,}$',
+              },
+              {
+                label: 'Bolivia',
+                pattern: '^(\\+591)[0-9]{9,}$',
+              },
+              {
+                label: 'Canada',
+                pattern: '^(\\+1)(206|224)[0-9]{1,}$',
+              },
+              {
+                label: 'United States',
+                pattern: '^(\\+1)[0-9]{1,}$',
               },
             ],
           },
