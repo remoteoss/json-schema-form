8d1ebfce7b28c560a3760b1bcccda6c269bbe7e9
brennj <jgabrennan@gmail.com>
2024-12-08 01:36:41 +0100

    chore: current progress

--- src/v2/tests/conditional-tests.ts
+++ src/v2/tests/conditional-tests.ts
@@ -1,28 +1,55 @@
 export const basicConditionalTestCases = [
+  //   {
+  //     title: 'Basic if/then condition',
+  //     schema: {
+  //       type: 'object',
+  //       properties: {
+  //         field: { type: 'string' },
+  //         otherField: { type: 'string' },
+  //       },
+  //       if: {
+  //         properties: {
+  //           field: { const: 'yes' },
+  //         },
+  //         required: ['field'],
+  //       },
+  //       then: {
+  //         required: ['otherField'],
+  //       },
+  //     } as const,
+  //     validTestCases: [{ data: { field: 'no' } }, { data: { field: 'yes', otherField: 'test' } }],
+  //     invalidTestCases: [
+  //       {
+  //         data: { field: 'yes' },
+  //         error: { otherField: 'Field is required' },
+  //       },
+  //     ],
+  //   },
   {
-    title: 'Basic if/then condition',
+    title: 'Basic if/then/else condition',
     schema: {
       type: 'object',
       properties: {
         field: { type: 'string' },
-        otherField: { type: 'string' },
+        number: { type: 'number' },
       },
       if: {
         properties: {
           field: { const: 'yes' },
         },
-        required: ['field'],
       },
       then: {
-        required: ['otherField'],
+        properties: {
+          number: { minimum: 10 },
+        },
+      },
+      else: {
+        properties: {
+          number: { minimum: 5 },
+        },
       },
     } as const,
-    validTestCases: [{ data: { field: 'no' } }, { data: { field: 'yes', otherField: 'test' } }],
-    invalidTestCases: [
-      {
-        data: { field: 'yes' },
-        error: { otherField: 'Field is required' },
-      },
-    ],
+    validTestCases: [{ data: { field: 'yes', number: 10 } }, { data: { field: 'no', number: 5 } }],
+    invalidTestCases: [{ data: { field: 'yes', number: 9 } }],
   },
 ];
