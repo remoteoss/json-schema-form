e6c7b70776bacf9cb8023978a1a70ed0aba7ba5f
brennj <jgabrennan@gmail.com>
2024-12-18 14:55:40 +0100

    chore: small refactor

--- src/v2/tests/properties.ts
+++ src/v2/tests/properties.ts
@@ -7,7 +7,25 @@ export const propertiesTestCases = [
         bar: { type: 'string' },
       },
     },
-    validTestCases: [{ data: { foo: 1, bar: 'baz' } }],
-    invalidTestCases: [{ data: { foo: 1, bar: {} } }],
+    validTestCases: [
+      { data: { foo: 1, bar: 'baz' } },
+      { data: { quux: [] } },
+      { data: [] },
+      { data: 12 },
+    ],
+    invalidTestCases: [{ data: { foo: 1, bar: {} } }, { data: { foo: [], bar: {} } }],
+  },
+  {
+    title: 'properties, patternProperties, additionalProperties interaction',
+    schema: {
+      properties: {
+        foo: { type: 'array', maxItems: 3 },
+        bar: { type: 'array' },
+      },
+      patternProperties: { 'f.o': { minItems: 2 } },
+      additionalProperties: { type: 'integer' },
+    },
+    validTestCases: [{ data: { foo: [1, 2] } }],
+    invalidTestCases: [{ data: { foo: [1, 2, 3, 4] } }, { data: { foo: [] } }],
   },
 ];
