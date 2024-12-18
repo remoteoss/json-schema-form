ca2ef58b7599b51e2b228db2f14cec04254b0b5d
brennj <jgabrennan@gmail.com>
2024-12-18 11:42:41 +0100

    chore: make required tests pass

--- src/v2/tests/required.ts
+++ src/v2/tests/required.ts
@@ -7,8 +7,53 @@ export const requiredTestCases = [
         bar: {},
       },
       required: ['foo'],
-    },
-    validTestCases: [{ data: { foo: 1 } }, { data: [] }],
+    } as const,
+    validTestCases: [{ data: { foo: 1 } }, { data: [] }, { data: '' }, { data: 12 }],
     invalidTestCases: [{ data: { bar: 1 } }],
+  },
+  {
+    title: 'required default validation',
+    schema: {
+      properties: {
+        foo: {},
+      },
+    },
+    validTestCases: [{ data: {} }],
+  },
+  {
+    title: 'required with empty array',
+    schema: {
+      properties: {
+        foo: {},
+      },
+      required: [],
+    },
+    validTestCases: [{ data: {} }],
+  },
+  {
+    title: 'required with escaped characters',
+    schema: {
+      required: [`foo\nbar`, `foo\"bar`, `foo\\bar`, `foo\rbar`, `foo\tbar`, `foo\fbar`],
+    },
+    validTestCases: [
+      {
+        data: {
+          'foo\nbar': 1,
+          'foo"bar': 1,
+          'foo\\bar': 1,
+          'foo\rbar': 1,
+          'foo\tbar': 1,
+          'foo\fbar': 1,
+        },
+      },
+    ],
+    invalidTestCases: [
+      {
+        data: {
+          'foo\nbar': '1',
+          'foo"bar': '1',
+        },
+      },
+    ],
   },
 ];
