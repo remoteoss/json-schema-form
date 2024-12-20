750efbab02443327ccbb65f6af9ffc85e65f669a
brennj <jgabrennan@gmail.com>
2024-12-20 17:14:45 +0100

    chore: full set of enum tests

--- src/v2/tests/enum.ts
+++ src/v2/tests/enum.ts
@@ -35,4 +35,66 @@ export const enumTests = [
       { data: {} },
     ],
   },
+  {
+    title: 'enum with escaped characters',
+    schema: {
+      enum: ['foo\nbar', 'foo\rbar'],
+    },
+    validTestCases: [{ data: 'foo\nbar' }, { data: 'foo\rbar' }],
+    invalidTestCases: [{ data: 'abc' }],
+  },
+  {
+    title: 'enum with false does not match 0',
+    schema: { enum: [false] },
+    validTestCases: [{ data: false }],
+    invalidTestCases: [{ data: 0 }, { data: 0.0 }],
+  },
+  {
+    title: 'enum with [false] does not match [0]',
+    schema: { enum: [[false]] },
+    validTestCases: [{ data: [false] }],
+    invalidTestCases: [{ data: [0] }, { data: [0.0] }],
+  },
+  {
+    title: 'enum with true does not match 1',
+    schema: { enum: [true] },
+    validTestCases: [{ data: true }],
+    invalidTestCases: [{ data: 1 }, { data: 1.0 }],
+  },
+  {
+    title: 'enum with [true] does not match [1]',
+    schema: { enum: [[true]] },
+    validTestCases: [{ data: [true] }],
+    invalidTestCases: [{ data: [1] }, { data: [1.0] }],
+  },
+  {
+    title: 'enum with 0 does not match false',
+    schema: { enum: [0] },
+    validTestCases: [{ data: 0 }, { data: 0.0 }],
+    invalidTestCases: [{ data: false }],
+  },
+  {
+    title: 'enum with [0] does not match [false]',
+    schema: { enum: [[0]] },
+    validTestCases: [{ data: [0] }, { data: [0.0] }],
+    invalidTestCases: [{ data: [false] }],
+  },
+  {
+    title: 'enum with 1 does not match true',
+    schema: { enum: [1] },
+    validTestCases: [{ data: 1 }, { data: 1.0 }],
+    invalidTestCases: [{ data: true }],
+  },
+  {
+    title: 'enum with [1] does not match [true]',
+    schema: { enum: [[1]] },
+    validTestCases: [{ data: [1] }, { data: [1.0] }],
+    invalidTestCases: [{ data: [true] }],
+  },
+  {
+    title: 'null characters in strings',
+    schema: { enum: ['hello\u0000there'] },
+    validTestCases: [{ data: 'hello\u0000there' }],
+    invalidTestCases: [{ data: 'hellothere' }],
+  },
 ];
