58b29b0f3b10299c3e71194475edb6598d825a09
brennj <jgabrennan@gmail.com>
2024-12-20 15:21:52 +0100

    chore: ifthenelse specced out

--- src/v2/tests/if-then-else.ts
+++ src/v2/tests/if-then-else.ts
@@ -52,4 +52,73 @@ export const ifThenElseTests = [
     validTestCases: [{ data: -1 }, { data: 4 }],
     invalidTestCases: [{ data: 3 }],
   },
+  {
+    title: 'validate against correct branch, then vs else',
+    schema: {
+      if: {
+        exclusiveMaximum: 0,
+      },
+      then: {
+        minimum: -10,
+      },
+      else: {
+        multipleOf: 2,
+      },
+    },
+    validTestCases: [{ data: -1 }, { data: 4 }],
+    invalidTestCases: [{ data: -100 }, { data: 3 }],
+  },
+  {
+    title: 'non-interference across combined schemas',
+    schema: {
+      allOf: [
+        {
+          if: {
+            exclusiveMaximum: 0,
+          },
+        },
+        {
+          then: {
+            minimum: -10,
+          },
+        },
+        {
+          else: {
+            multipleOf: 2,
+          },
+        },
+      ],
+    },
+    validTestCases: [{ data: -100 }, { data: 3 }],
+  },
+  {
+    title: 'if with boolean schema true',
+    schema: {
+      if: true,
+      then: { const: 'then' },
+      else: { const: 'else' },
+    },
+    validTestCases: [{ data: 'then' }],
+    invalidTestCases: [{ data: 'else' }],
+  },
+  {
+    title: 'if with boolean schema false',
+    schema: {
+      if: false,
+      then: { const: 'then' },
+      else: { const: 'else' },
+    },
+    validTestCases: [{ data: 'else' }],
+    invalidTestCases: [{ data: 'then' }],
+  },
+  {
+    title: 'if appears at the end when serialized (keyword processing sequence)',
+    schema: {
+      then: { const: 'yes' },
+      else: { const: 'other' },
+      if: { maxLength: 4 },
+    },
+    validTestCases: [{ data: 'yes' }, { data: 'other' }],
+    invalidTestCases: [{ data: 'no' }, { data: 'invalid' }],
+  },
 ];
