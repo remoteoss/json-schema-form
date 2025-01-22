c0725661f4f239c62a2e303ecb50b4fc818f2f2f
Thien Do <thien@remote.com>
2025-01-22 14:34:13 +0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,13 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
+    ['@babel/preset-env', {
+      targets: { node: 'current' },
+    }],
   ],
   plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        alias: {
-          '@': './src',
-        },
-      },
-    ],
+    ['module-resolver', {
+      root: ['./src'],
+      alias: { '@': './src' },
+    }],
   ],
 };
