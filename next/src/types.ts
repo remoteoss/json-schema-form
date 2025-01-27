9721c18cd9bcc9963dee5001712ee2455e5dda70
Luka Dornhecker <luka.dornhecker@hey.com>
2025-01-27 17:36:09 +0100

    chore(next): define JsfSchema type extending JSONSchema (#116)
    * define JsfSchema type extending JSONSchema
    
    ---------
    
    Co-authored-by: Luka Dornhecker <luka.dornhecker@remote.com>

---
+++ next/src/types.ts
@@ -0,0 +1,10 @@
+import type { JSONSchema } from 'json-schema-typed/draft-2020-12'
+
+export type JsfSchema = Exclude<JSONSchema, boolean> & {
+  'properties'?: Record<string, JsfSchema> | boolean
+  'x-jsf-logic'?: {
+    validations: Record<string, object>
+    computedValues: Record<string, object>
+  }
+  'x-jsf-order'?: string[]
+}
