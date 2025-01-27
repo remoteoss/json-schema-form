4efdbbf4e78dc6a273314267b6ec7ea4735e1f14
Luka Dornhecker <luka.dornhecker@remote.com>
2025-01-27 16:54:08 +0100

    override properties allowing them to have `x-jsf-*` keys as well

--- next/src/types.ts
+++ next/src/types.ts
@@ -1,6 +1,7 @@
 import type { JSONSchema } from 'json-schema-typed/draft-2020-12'
 
 export type JsfSchema = Exclude<JSONSchema, boolean> & {
+  'properties'?: Record<string, JsfSchema> | boolean
   'x-jsf-logic'?: {
     validations: Record<string, object>
     computedValues: Record<string, object>
