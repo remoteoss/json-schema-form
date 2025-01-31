d11dc6793b74ab57d2c112d212a6ce3e7ec011a4
David Dragovacz <david.dragovacz@remote.com>
2025-01-31 15:48:16 +0000

    build(next): correcting version check

--- scripts/pr_next_dev_version.js
+++ scripts/pr_next_dev_version.js
@@ -5,7 +5,7 @@ function init() {
   const packageJson = fs.readFileSync(path.resolve(__dirname, '../next/package.json'), 'utf8');
   const { version } = JSON.parse(packageJson);
 
-  if (!version.includes('-dev')) {
+  if (version.includes('-dev')) {
     console.log(
       `🟠 This PR cannot be merged because the next/package.json version ${version} contains "-dev".` +
         '\n   The version in next/package.json should be a beta version (e.g., 1.0.0-beta.0).'
@@ -13,8 +13,7 @@ function init() {
     process.exit(1);
   }
 
-  // Making this fail on purpose to test the CI
-  if (version.includes('-beta.')) {
+  if (!version.includes('-beta.')) {
     console.log(
       `🟠 This PR cannot be merged because the next/package.json version ${version} is not a beta version.` +
         '\n   The version in next/package.json should be in format X.X.X-beta.Y (e.g., 1.0.0-beta.0).'
