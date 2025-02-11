d8beb8fabc8672998f18df70a7e65213ab95da48
David Dragovacz <david.dragovacz@remote.com>
2025-02-11 10:05:06 +0000

    chore: fix lint script and add proper type check
    - we had quite a few type errors in the test folder that we missed
    - linter was checking every folder including the json-schema-test-suite which we don't want
    - we were missing the `module` from `package.json` resulting in a warning in the console when running tests

--- next/eslint.config.mjs
+++ next/eslint.config.mjs
@@ -1,3 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({})
+export default antfu({
+  ignores: [
+    // Build output
+    'dist',
+
+    // Dependencies
+    'node_modules',
+
+    // External test suite
+    'json-schema-test-suite',
+  ],
+})
