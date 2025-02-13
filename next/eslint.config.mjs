afd4fc2af2b1f768a054639dc3eb62c3a52c0f08
David Dragovacz <david.dragovacz@remote.com>
2025-02-13 11:12:01 +0000

    adjust eslint config

--- next/eslint.config.mjs
+++ next/eslint.config.mjs
@@ -1,14 +1,3 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  ignores: [
-    // Build output
-    'dist',
-
-    // Dependencies
-    'node_modules',
-
-    // External test suite
-    'json-schema-test-suite',
-  ],
-})
+export default antfu({})
