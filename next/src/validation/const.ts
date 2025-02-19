f1ced694f6d3bad4126a860b1dcd0b6ea8054a62
Luka Dornhecker <luka.dornhecker@hey.com>
2025-02-19 11:12:36 +0100

    add comments

--- next/src/validation/const.ts
+++ next/src/validation/const.ts
@@ -1,6 +1,16 @@
 import type { ValidationError } from '../form'
 import type { NonBooleanJsfSchema, SchemaValue } from '../types'
 
+/**
+ * Compare two values for deep equality
+ * @param a - The first value to compare
+ * @param b - The second value to compare
+ * @returns `true` if the values are equal, `false` otherwise
+ * @description This function is compares two values for deep equality.
+ * Primitives are compared using strict equality.
+ * Arrays are compared element by element using recursion.
+ * Objects are compared key by key using recursion.
+ */
 function deepEqual(a: any, b: any): boolean {
   if (typeof a !== typeof b) {
     return false
@@ -43,6 +53,18 @@ function deepEqual(a: any, b: any): boolean {
   return false
 }
 
+/**
+ * Validate that a value is equal to a constant
+ * @param value - The value to validate
+ * @param schema - The schema to validate against
+ * @param path - The path to the value
+ * @returns An array of validation errors
+ * @description This function validates that a value is equal to a constant.
+ * The constant is defined in the `const` property of the schema.
+ * The value is validated against the constant using deep equality.
+ * @see https://json-schema.org/understanding-json-schema/reference/const
+ * @see https://json-schema.org/draft/2020-12/json-schema-validation#name-enum
+ */
 export function validateConst(value: SchemaValue, schema: NonBooleanJsfSchema, path: string[] = []): ValidationError[] {
   if (schema.const === undefined) {
     return []
