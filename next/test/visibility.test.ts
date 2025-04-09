37f1341621e6c9869c71b9d2e3d78f9acbf193a4
Capelo <antonio.c.capelo@gmail.com>
2025-04-09 16:37:12 +0100

    chore: remove unnecessary describe block

--- next/test/visibility.test.ts
+++ next/test/visibility.test.ts
@@ -151,56 +151,6 @@ describe('Field visibility', () => {
   })
 
   // This does not work with v0 but I think it should work with v1
-  describe('if on object schema level', () => {
-    const schema: JsfObjectSchema = {
-      type: 'object',
-      properties: {
-        name: {
-          type: 'string',
-        },
-        password: {
-          type: 'string',
-        },
-      },
-      if: {
-        properties: {
-          name: {
-            const: 'admin',
-          },
-        },
-        required: ['name'],
-      },
-      else: {
-        properties: {
-          password: false,
-        },
-      },
-    }
-
-    it('should hide the password field by default', () => {
-      const form = createHeadlessForm(schema, { initialValues: { name: '', password: null } })
-      // No name provided
-      expect(form.fields.find(field => field.name === 'password')?.isVisible).toBe(false)
-
-      // Different name provided
-      form.handleValidation({
-        name: 'some name',
-        password: null,
-      })
-      expect(form.fields.find(field => field.name === 'password')?.isVisible).toBe(false)
-    })
-
-    it('should show the password field if the name is admin', () => {
-      const form = createHeadlessForm(schema)
-      form.handleValidation({
-        name: 'admin',
-        password: null,
-      })
-      expect(form.fields.find(field => field.name === 'password')?.isVisible).toBe(true)
-    })
-  })
-
-  // This does not work with v0 but I think it should work with v1
   describe('if on a fieldset schema level', () => {
     const schema: JsfObjectSchema = {
       type: 'object',
