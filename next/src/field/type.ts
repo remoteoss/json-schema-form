ac2b75998991023217ab4313a4e3e20461bae326
Joao Almeida <joao.mnp.almeida@gmail.com>
2025-05-08 17:35:39 +0100

    create TS type for each field

--- next/src/field/type.ts
+++ next/src/field/type.ts
@@ -1,47 +1,123 @@
 import type { JsfSchemaType } from '../types'
 
-/**
- * WIP type for UI field output that allows for all `x-jsf-presentation` properties to be splatted
- * TODO/QUESTION: what are the required fields for a field? what are the things we want to deprecate, if any?
- */
-export interface Field {
-  name: string
-  label?: string
-  description?: string
-  fields?: Field[]
-  // @deprecated in favor of inputType,
+export type FieldType = 'text' | 'number' | 'select' | 'file' | 'radio' | 'group-array' | 'email' | 'date' | 'checkbox' | 'fieldset' | 'money' | 'country' | 'textarea'
+
+interface BaseField {
   type: FieldType
-  inputType: FieldType
+  name: string
+  label: string
   required: boolean
+  inputType: FieldType
   jsonType: JsfSchemaType
-  isVisible: boolean
-  accept?: string
-  errorMessage?: Record<string, string>
   computedAttributes?: Record<string, unknown>
+  errorMessage: Record<string, string>
+  schema: any
+  isVisible: boolean
+  description?: string
+  statement?: {
+    title: string
+    inputType: 'statement'
+    severity: 'warning' | 'error' | 'info'
+  }
+  [key: string]: unknown
+}
+
+export interface FieldOption {
+  label: string
+  value: string
+  description?: string
+}
+
+export interface FieldSelect extends BaseField {
+  type: 'select'
+  options: FieldOption[]
+}
+
+export interface FieldTextarea extends BaseField {
+  type: 'textarea'
+  maxLength: number
+  minLength?: number
+}
+
+export interface FieldDate extends BaseField {
+  type: 'date'
+  format: string
   minDate?: string
   maxDate?: string
   maxLength?: number
-  maxFileSize?: number
-  format?: string
-  anyOf?: unknown[]
-  options?: unknown[]
-  const?: unknown
-  checkboxValue?: unknown
-
-  // Allow additional properties from x-jsf-presentation (e.g. meta from oneOf/anyOf)
-  [key: string]: unknown
 }
 
-/**
- * Field option
- * @description
- * Represents a key/value pair that is used to populate the options for a field.
- * Will be created from the oneOf/anyOf elements in a schema.
- */
-export interface FieldOption {
+export interface FieldText extends BaseField {
+  type: 'text'
+  maxLength: number
+  maskSecret?: number
+}
+
+export interface FieldRadio extends BaseField {
+  type: 'radio'
+  options: FieldOption[]
+  direction?: 'row' | 'column'
+  const?: string
+}
+
+export interface FieldNumber extends BaseField {
+  type: 'number'
+}
+
+export interface FieldMoney extends BaseField {
+  type: 'money'
+  currency: string
+}
+
+export interface FieldCheckbox extends BaseField {
+  type: 'checkbox'
+  options?: FieldOption[]
+  multiple?: boolean
+  direction?: 'row' | 'column'
+  checkboxValue?: string | boolean
+  const?: string
+}
+
+export interface FieldEmail extends BaseField {
+  type: 'email'
+  maxLength: number
+  format: 'email'
+}
+
+export interface FieldFile extends BaseField {
+  type: 'file'
+  accept: string
+  multiple?: boolean
+  fileDownload: string
+  fileName: string
+}
+export interface FieldFieldSet extends BaseField {
+  type: 'fieldset'
+  valueGroupingDisabled?: boolean
+  visualGroupingDisabled?: boolean
+  variant?: 'card' | 'focused' | 'default'
+  fields: Field[]
+}
+
+export interface GroupArrayField extends BaseField {
+  type: 'group-array'
+  name: string
   label: string
-  value: unknown
-  [key: string]: unknown
+  description: string
+  fields: () => Field[]
+  addFieldText: string
 }
 
-export type FieldType = 'text' | 'number' | 'select' | 'file' | 'radio' | 'group-array' | 'email' | 'date' | 'checkbox' | 'fieldset' | 'money' | 'country' | 'textarea'
+export type Field =
+  | FieldSelect
+  | FieldTextarea
+  | FieldDate
+  | FieldText
+  | FieldRadio
+  | FieldNumber
+  | FieldMoney
+  | FieldCheckbox
+  | FieldEmail
+  | FieldFile
+  | FieldFieldSet
+  | GroupArrayField
