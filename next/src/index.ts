92af09b0b892d32a60c2bd7695137f293888422b
Gabriel Garcia <gabriel.garcia@remote.com>
2025-05-12 16:33:00 +0200

    export fields

--- next/src/index.ts
+++ next/src/index.ts
@@ -1,2 +1,9 @@
-export { createHeadlessForm, CreateHeadlessFormOptions, ValidationOptions } from './form'
+export { type Field, type FieldType } from './field/type'
+export {
+  createHeadlessForm,
+  CreateHeadlessFormOptions,
+  type FormErrors,
+  ValidationOptions,
+  type ValidationResult,
+} from './form'
 export { modifySchema as modify } from './modify-schema'
