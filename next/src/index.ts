cda1c3a6927689063b08d70a42f4d2305dedc0f2
Gabriel García Seco <ggarciaseco@gmail.com>
2025-05-19 10:27:12 +0200

    fix(next) - fix types property in package.json (#186)
    * fix(next) - fix types property in package.json
    
    * export fields
    
    * add type keyword
    
    * remove lock
    
    ---------
    
    Co-authored-by: Gabriel Garcia <gabriel.garcia@remote.com>

--- next/src/index.ts
+++ next/src/index.ts
@@ -1,2 +1,9 @@
-export { createHeadlessForm, CreateHeadlessFormOptions, ValidationOptions } from './form'
+export { type Field, type FieldType } from './field/type'
+export {
+  createHeadlessForm,
+  type CreateHeadlessFormOptions,
+  type FormErrors,
+  type ValidationOptions,
+  type ValidationResult,
+} from './form'
 export { modifySchema as modify } from './modify-schema'
