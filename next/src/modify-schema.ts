e1f7922f22a420b39b8133946167f2ad1d9dfa57
Capelo <capelo@remote.com>
2025-07-08 15:02:08 +0100

    fix: fix lint

--- next/src/modify-schema.ts
+++ next/src/modify-schema.ts
@@ -90,7 +90,7 @@ function isConditionalReferencingAnyPickedField(condition: JsfSchema, fieldsToPi
 
   const inThen
     = intersection(thenCondition?.required || [], fieldsToPick)
-    || intersection(Object.keys(thenCondition?.properties || {}), fieldsToPick)
+      || intersection(Object.keys(thenCondition?.properties || {}), fieldsToPick)
 
   if (inThen.length > 0) {
     return true
@@ -98,7 +98,7 @@ function isConditionalReferencingAnyPickedField(condition: JsfSchema, fieldsToPi
 
   const inElse
     = intersection(elseCondition?.required || [], fieldsToPick)
-    || intersection(Object.keys(elseCondition?.properties || {}), fieldsToPick)
+      || intersection(Object.keys(elseCondition?.properties || {}), fieldsToPick)
 
   if (inElse.length > 0) {
     return true
