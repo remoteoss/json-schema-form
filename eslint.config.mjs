750947b565c1b230df3f8faae246f501ce971998
Luka Dornhecker <luka.dornhecker@hey.com>
2025-08-27 14:28:37 +0200

    add json-schema-test-suite to eslint ignores (#228)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['test/v0-baseline-test-results.json', 'v0/**', 'scripts/**'],
+  ignores: [
+    'test/v0-baseline-test-results.json',
+    'v0/**',
+    'scripts/**',
+    'json-schema-test-suite/**',
+  ],
   markdown: false,
   modules: false,
   rules: {
